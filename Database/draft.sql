f48b10300f63bd25b0b010cc5f71105f8fdf86cc
vedant Rajendra Yadav <vedantyadav799@gmail.com>
2023-04-29 23:38:22 +0530

    all tables tested successfully

--- Database/draft.sql
+++ Database/draft.sql
@@ -3,90 +3,12 @@ create database inventorymanagement;
 use inventorymanagement;
 
 -- Table Creation
-create table employees(employee_id INT NOT NULL auto_increment primary KEY,empfirst_name VARCHAR(100),emplast_name VARCHAR(50),birth_date DATETIME,hire_date DATETIME,contact_number VARCHAR(20),email VARCHAR(50),password VARCHAR(15) NOT NULL,photo varchar (50),gender VARCHAR(50),role_id int not null,constraint fk_role foreign key(role_id) references roles(role_id) on update cascade on delete cascade);
 CREATE TABLE roles(role_id INT NOT NULL AUTO_INCREMENT PRIMARY KEY, role varchar(50));
-CREATE TABLE warehouses(warehouse_id INT NOT NULL AUTO_INCREMENT primary KEY,warehouse_name VARCHAR(20),sections_id int not null,constraint fk_sections foreign key(sections_id) references sections(section_id) on update cascade on delete cascade);
-create table sections(section_id INT NOT NULL AUTO_INCREMENT primary KEY,section_name VARCHAR(20), floors_id int not null,constraint fk_floors foreign key(floors_id) references floors(floor_id) on update cascade on delete cascade);
-create table floors(floor_id INT NOT NULL AUTO_INCREMENT primary KEY, floor_number varchar(20), mid int not null,constraint fk_mid foreign key(mid) references materials(material_id) on update cascade on delete cascade);
+create table employees(employee_id INT NOT NULL auto_increment primary KEY,empfirst_name VARCHAR(100),emplast_name VARCHAR(50),birth_date DATETIME,hire_date DATETIME,contact_number VARCHAR(20),email VARCHAR(50),password VARCHAR(15) NOT NULL,photo varchar (50),gender VARCHAR(50),role_id int not null,constraint fk_role foreign key(role_id) references roles(role_id) on update cascade on delete cascade);
 CREATE TABLE materials(material_id INT NOT NULL AUTO_INCREMENT primary KEY, material_name VARCHAR(100),material_type VARCHAR(100),quantity INT NOT NULL,unit_price INT NOT NULL, photo varchar (50));
-
--- Insertion for warehouses
-insert into warehouses(warehouse_name, sections_id) values ('warehouse 1', 1);
-insert into warehouses(warehouse_name, sections_id) values ('warehouse 1', 2);
-insert into warehouses(warehouse_name, sections_id) values ('warehouse 1', 3);
-insert into warehouses(warehouse_name, sections_id) values ('warehouse 1', 4);
-insert into warehouses(warehouse_name, sections_id) values ('warehouse 1', 5);
-insert into warehouses(warehouse_name, sections_id) values ('warehouse 1', 6);
-insert into warehouses(warehouse_name, sections_id) values ('warehouse 1', 7);
-insert into warehouses(warehouse_name, sections_id) values ('warehouse 1', 8);
-insert into warehouses(warehouse_name, sections_id) values ('warehouse 1', 9);
-insert into warehouses(warehouse_name, sections_id) values ('warehouse 1', 10);
-insert into warehouses(warehouse_name, sections_id) values ('warehouse 1', 11);
-insert into warehouses(warehouse_name, sections_id) values ('warehouse 1', 12);
-insert into warehouses(warehouse_name, sections_id) values ('warehouse 2', 13);
-insert into warehouses(warehouse_name, sections_id) values ('warehouse 2', 14);
-insert into warehouses(warehouse_name, sections_id) values ('warehouse 2', 15);
-insert into warehouses(warehouse_name, sections_id) values ('warehouse 2', 16);
-insert into warehouses(warehouse_name, sections_id) values ('warehouse 2', 17);
-insert into warehouses(warehouse_name, sections_id) values ('warehouse 2', 18);
-insert into warehouses(warehouse_name, sections_id) values ('warehouse 2', 19);
-insert into warehouses(warehouse_name, sections_id) values ('warehouse 2', 20);
-insert into warehouses(warehouse_name, sections_id) values ('warehouse 2', 21);
-insert into warehouses(warehouse_name, sections_id) values ('warehouse 2', 22);
-insert into warehouses(warehouse_name, sections_id) values ('warehouse 2', 23);
-insert into warehouses(warehouse_name, sections_id) values ('warehouse 2', 24);
-
--- Insertion for Sections
-insert into sections(section_name, floors_id)values('Section 1', 1);
-insert into sections(section_name, floors_id)values('Section 1', 2);
-insert into sections(section_name, floors_id)values('Section 1', 3);
-insert into sections(section_name, floors_id)values('Section 2', 4);
-insert into sections(section_name, floors_id)values('Section 2', 5);
-insert into sections(section_name, floors_id)values('Section 2', 6);
-insert into sections(section_name, floors_id)values('Section 3', 7);
-insert into sections(section_name, floors_id)values('Section 3', 8);
-insert into sections(section_name, floors_id)values('Section 3', 9);
-insert into sections(section_name, floors_id)values('Section 4', 10);
-insert into sections(section_name, floors_id)values('Section 4', 11);
-insert into sections(section_name, floors_id)values('Section 4', 12);
-insert into sections(section_name, floors_id)values('Section 5', 13);
-insert into sections(section_name, floors_id)values('Section 5', 14);
-insert into sections(section_name, floors_id)values('Section 5', 15);
-insert into sections(section_name, floors_id)values('Section 6', 16);
-insert into sections(section_name, floors_id)values('Section 6', 17);
-insert into sections(section_name, floors_id)values('Section 6', 18);
-insert into sections(section_name, floors_id)values('Section 7', 19);
-insert into sections(section_name, floors_id)values('Section 7', 20);
-insert into sections(section_name, floors_id)values('Section 7', 21);
-insert into sections(section_name, floors_id)values('Section 8', 22);
-insert into sections(section_name, floors_id)values('Section 8', 23);
-insert into sections(section_name, floors_id)values('Section 8', 24);
-
--- Insertion for floors
-insert into floors(floor_number,mid) values('A1',1);
-insert into floors(floor_number,mid) values('A2',2);
-insert into floors(floor_number,mid) values('A3',3);
-insert into floors(floor_number,mid) values('B1',4);
-insert into floors(floor_number,mid) values('B2',5);
-insert into floors(floor_number,mid) values('B3',6);
-insert into floors(floor_number,mid) values('C1',7);
-insert into floors(floor_number,mid) values('C2',8);
-insert into floors(floor_number,mid) values('C3',9);
-insert into floors(floor_number,mid) values('D1',10);
-insert into floors(floor_number,mid) values('D2',11);
-insert into floors(floor_number,mid) values('D3',12);
-insert into floors(floor_number,mid) values('E1',13);
-insert into floors(floor_number,mid) values('E2',14);
-insert into floors(floor_number,mid) values('E3',15);
-insert into floors(floor_number,mid) values('F1',16);
-insert into floors(floor_number,mid) values('F2',17);
-insert into floors(floor_number,mid) values('F3',18);
-insert into floors(floor_number,mid) values('G1',19);
-insert into floors(floor_number,mid) values('G2',20);
-insert into floors(floor_number,mid) values('G3',21);
-insert into floors(floor_number,mid) values('H1',22);
-insert into floors(floor_number,mid) values('H2',23);
-insert into floors(floor_number,mid) values('H3',24);
+create table floors(floor_id INT NOT NULL AUTO_INCREMENT primary KEY, floor_number varchar(20), mid int not null,constraint fk_mid foreign key(mid) references materials(material_id) on update cascade on delete cascade);
+create table sections(section_id INT NOT NULL AUTO_INCREMENT primary KEY,section_name VARCHAR(20), floors_id int not null,constraint fk_floors foreign key(floors_id) references floors(floor_id) on update cascade on delete cascade);
+CREATE TABLE warehouses(warehouse_id INT NOT NULL AUTO_INCREMENT primary KEY,warehouse_name VARCHAR(20),sections_id int not null,constraint fk_sections foreign key(sections_id) references sections(section_id) on update cascade on delete cascade);
 
 -- Insertion for material
 insert into materials(material_name, material_type, quantity, unit_price, photo) values ('Needle Bearing','Bearing',784,20, './images/SM.jpg');
@@ -114,6 +36,87 @@ insert into materials(material_name, material_type, quantity, unit_price, photo)
 insert into materials(material_name, material_type, quantity, unit_price, photo) values ('GB400 Housing','Housing',78,2500, './images/SM.jpg');
 insert into materials(material_name, material_type, quantity, unit_price, photo) values ('Gb540 Housing','Housing',90,3500, './images/SM.jpg');
 
+-- Insertion for floors
+insert into floors(floor_number,mid) values('A1',1);
+insert into floors(floor_number,mid) values('A2',2);
+insert into floors(floor_number,mid) values('A3',3);
+insert into floors(floor_number,mid) values('B1',4);
+insert into floors(floor_number,mid) values('B2',5);
+insert into floors(floor_number,mid) values('B3',6);
+insert into floors(floor_number,mid) values('C1',7);
+insert into floors(floor_number,mid) values('C2',8);
+insert into floors(floor_number,mid) values('C3',9);
+insert into floors(floor_number,mid) values('D1',10);
+insert into floors(floor_number,mid) values('D2',11);
+insert into floors(floor_number,mid) values('D3',12);
+insert into floors(floor_number,mid) values('E1',13);
+insert into floors(floor_number,mid) values('E2',14);
+insert into floors(floor_number,mid) values('E3',15);
+insert into floors(floor_number,mid) values('F1',16);
+insert into floors(floor_number,mid) values('F2',17);
+insert into floors(floor_number,mid) values('F3',18);
+insert into floors(floor_number,mid) values('G1',19);
+insert into floors(floor_number,mid) values('G2',20);
+insert into floors(floor_number,mid) values('G3',21);
+insert into floors(floor_number,mid) values('H1',22);
+insert into floors(floor_number,mid) values('H2',23);
+insert into floors(floor_number,mid) values('H3',24);
+
+
+-- Insertion for Sections
+insert into sections(section_name, floors_id)values('Section 1', 1);
+insert into sections(section_name, floors_id)values('Section 1', 2);
+insert into sections(section_name, floors_id)values('Section 1', 3);
+insert into sections(section_name, floors_id)values('Section 2', 4);
+insert into sections(section_name, floors_id)values('Section 2', 5);
+insert into sections(section_name, floors_id)values('Section 2', 6);
+insert into sections(section_name, floors_id)values('Section 3', 7);
+insert into sections(section_name, floors_id)values('Section 3', 8);
+insert into sections(section_name, floors_id)values('Section 3', 9);
+insert into sections(section_name, floors_id)values('Section 4', 10);
+insert into sections(section_name, floors_id)values('Section 4', 11);
+insert into sections(section_name, floors_id)values('Section 4', 12);
+insert into sections(section_name, floors_id)values('Section 5', 13);
+insert into sections(section_name, floors_id)values('Section 5', 14);
+insert into sections(section_name, floors_id)values('Section 5', 15);
+insert into sections(section_name, floors_id)values('Section 6', 16);
+insert into sections(section_name, floors_id)values('Section 6', 17);
+insert into sections(section_name, floors_id)values('Section 6', 18);
+insert into sections(section_name, floors_id)values('Section 7', 19);
+insert into sections(section_name, floors_id)values('Section 7', 20);
+insert into sections(section_name, floors_id)values('Section 7', 21);
+insert into sections(section_name, floors_id)values('Section 8', 22);
+insert into sections(section_name, floors_id)values('Section 8', 23);
+insert into sections(section_name, floors_id)values('Section 8', 24);
+
+
+-- Insertion for warehouses
+insert into warehouses(warehouse_name, sections_id) values ('warehouse 1', 1);
+insert into warehouses(warehouse_name, sections_id) values ('warehouse 1', 2);
+insert into warehouses(warehouse_name, sections_id) values ('warehouse 1', 3);
+insert into warehouses(warehouse_name, sections_id) values ('warehouse 1', 4);
+insert into warehouses(warehouse_name, sections_id) values ('warehouse 1', 5);
+insert into warehouses(warehouse_name, sections_id) values ('warehouse 1', 6);
+insert into warehouses(warehouse_name, sections_id) values ('warehouse 1', 7);
+insert into warehouses(warehouse_name, sections_id) values ('warehouse 1', 8);
+insert into warehouses(warehouse_name, sections_id) values ('warehouse 1', 9);
+insert into warehouses(warehouse_name, sections_id) values ('warehouse 1', 10);
+insert into warehouses(warehouse_name, sections_id) values ('warehouse 1', 11);
+insert into warehouses(warehouse_name, sections_id) values ('warehouse 1', 12);
+insert into warehouses(warehouse_name, sections_id) values ('warehouse 2', 13);
+insert into warehouses(warehouse_name, sections_id) values ('warehouse 2', 14);
+insert into warehouses(warehouse_name, sections_id) values ('warehouse 2', 15);
+insert into warehouses(warehouse_name, sections_id) values ('warehouse 2', 16);
+insert into warehouses(warehouse_name, sections_id) values ('warehouse 2', 17);
+insert into warehouses(warehouse_name, sections_id) values ('warehouse 2', 18);
+insert into warehouses(warehouse_name, sections_id) values ('warehouse 2', 19);
+insert into warehouses(warehouse_name, sections_id) values ('warehouse 2', 20);
+insert into warehouses(warehouse_name, sections_id) values ('warehouse 2', 21);
+insert into warehouses(warehouse_name, sections_id) values ('warehouse 2', 22);
+insert into warehouses(warehouse_name, sections_id) values ('warehouse 2', 23);
+insert into warehouses(warehouse_name, sections_id) values ('warehouse 2', 24);
+
+
 -- Insertion for roles
 insert INTO roles(role) VALUES("Incharge");
 INSERT INTO roles(role) VALUES("Store Manager");
@@ -126,3 +129,23 @@ INSERT INTO employees(empfirst_name,emplast_name,birth_date,hire_date,contact_nu
 INSERT INTO employees(empfirst_name,emplast_name,birth_date,hire_date,contact_number, role_id,email,password,photo,gender)VALUES('Siddhesh','Pandit','1996-01-02','2021-09-12','7845967845', 3 ,'SP@gmail.com','SP789956' ,'./images/SM.jpg', 'Male');
 INSERT INTO employees(empfirst_name,emplast_name,birth_date,hire_date,contact_number, role_id,email,password,photo,gender)VALUES('Tejaswini','Salvi','1992-11-19','2020-09-01','9888754415', 3 ,'TS22@gmail.com','TS337845' ,'./images/SM.jpg', 'Female');
 INSERT INTO employees(empfirst_name,emplast_name,birth_date,hire_date,contact_number, role_id,email,password,photo,gender)VALUES('Vedant','Yadav','1987-01-07','2009-03-11','99887564123', 1 ,'VY@gmail.com','VY788814' ,'./images/SM.jpg', 'Male');
+
+-- employees and their role
+select employees.empfirst_name, employees.emplast_name, roles.role
+from employees
+Inner join roles on employees.role_id= roles.role_id;  
+
+-- sections in warehouse
+SELECT warehouses.warehouse_name, sections.section_name
+FROM warehouses
+INNER JOIN sections ON  warehouses.sections_id=  sections.section_id;
+
+-- floors in section
+SELECT sections.section_name, floors.floor_number
+FROM sections
+INNER JOIN floors ON  sections.floors_id=  floors.floor_id;
+
+-- materials in floor
+SELECT floors.floor_number, materials.material_name, materials.material_type
+FROM floors
+INNER JOIN materials ON  floors.mid=  materials.material_id;
