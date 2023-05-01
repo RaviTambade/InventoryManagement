2ad742dc6cf84fbbe1104fb01fd486985147d4b9
vedant Rajendra Yadav <vedantyadav799@gmail.com>
2023-05-01 12:55:25 +0530

    new updates

--- InventoryManagement/Controllers/WarehousesCotroller.cs
+++ InventoryManagement/Controllers/WarehousesCotroller.cs
@@ -8,7 +8,27 @@ public class WarehousesController : ControllerBase
 {
     public IEnumerable<Material> GetAll()
     {
-        
+
+    } 
+
+    public IEnumerable<Material> GetById()
+    {
+           
+    } 
+
+    public IEnumerable<Material> Insert()
+    {
+           
+    } 
+
+    public IEnumerable<Material> Update()
+    {
+           
+    } 
+
+    public IEnumerable<Material> Delete()
+    {
+           
     }     
 
 }
