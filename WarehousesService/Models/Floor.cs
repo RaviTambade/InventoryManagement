a59af454737abaf3b6cc08b49f74505d677d6e98
vedant Rajendra Yadav <vedantyadav799@gmail.com>
2023-05-07 23:55:57 +0530

    material and floor model added

---
+++ WarehousesService/Models/Floor.cs
@@ -0,0 +1,5 @@
+namespace WarehousesService.Models;
+public class Floor{
+    public int FloorId{get;set;}
+    public string FloorName{get;set;}
+}
---
+++ WarehousesService/Models/Material.cs
@@ -0,0 +1,10 @@
+namespace WarehousesService.Models;
+public class Material{
+    public int MaterialId{get;set;}
+    public string MaterialName{get;set;}
+    public string MaterialType{get;set;}
+    public int MaterialQuantity{get;set;}
+    public int MaterialUnitPrice{get;set;}
+    public string MaterialImgUrl{get;set;}
+    
+}
