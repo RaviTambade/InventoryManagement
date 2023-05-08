ad72c7f22964645727f9c1b101cafb720c5a9933
vedant Rajendra Yadav <vedantyadav799@gmail.com>
2023-05-09 01:13:34 +0530

    login page added

--- Angular template/src/app/notifications/notifications.component.ts
+++ Angular template/src/app/notifications/notifications.component.ts
@@ -1,15 +1,28 @@
 import { Component, OnInit } from '@angular/core';
+import { AppService } from 'app/app.service';
 declare var $: any;
 @Component({
   selector: 'app-notifications',
   templateUrl: './notifications.component.html',
   styleUrls: ['./notifications.component.css']
 })
-export class NotificationsComponent implements OnInit {
+export class NotificationsComponent  {
 
-  constructor() { }
+  login={
+    "email":'',
+    "password":''
+  };
+  status: boolean | undefined;
 
-  ngOnInit() {
+  constructor(private svc:AppService) { }
+
+ 
+
+  Login(_loginForm:any){
+    this.svc.Login(this.login).subscribe((Response)=>{
+      this.status=Response;
+      console.log(Response);
+    })
   }
 
 }
