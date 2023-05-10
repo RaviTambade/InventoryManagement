8738b7df45cb1e7c635eebdd1b6827f5e4e86c64
vedant Rajendra Yadav <vedantyadav799@gmail.com>
2023-05-10 16:35:54 +0530

    create div to store material info. hardcoded data

---
+++ Angular template/src/app/Material.ts
@@ -0,0 +1,11 @@
+export class Login{
+    constructor(
+        public id:number,
+        public materialName:string,
+        public materialType:string,
+        public quantity:number,
+        public unitprice:number,
+        public imgurl:string,
+
+    ){}
+}
