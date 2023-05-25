48380fcf6ca21f11ad0f6995e075e9460513a274
vedant Rajendra Yadav <vedantyadav799@gmail.com>
2023-05-26 01:15:03 +0530

    modification in auth service

--- AuthService/Program.cs
+++ AuthService/Program.cs
@@ -14,8 +14,8 @@ builder.Services.AddCors(options =>
                           policy =>
                           {
                               policy.WithOrigins("http://localhost:4200")
-                                                  .AllowAnyHeader()
-                                                  .AllowAnyMethod();
+                              .AllowAnyHeader()
+                              .AllowAnyMethod();
                           });
 });
 // Add services to the container.
