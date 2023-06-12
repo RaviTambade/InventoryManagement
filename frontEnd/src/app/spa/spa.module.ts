842f21dba6dfc745ef2a97b0ea3054efaaafba85
VedantYadav799 <Vedantyadav799@gmail.com>
2023-06-12 11:37:57 +0530

    new components added

--- frontEnd/src/app/spa/spa.module.ts
+++ frontEnd/src/app/spa/spa.module.ts
@@ -1,12 +1,22 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterContainerComponent } from './router-container/router-container.component';
+import { HomeComponent } from './home/home.component';
+import { EmployeesComponent } from './employees/employees.component';
+import { MaterialsComponent } from './materials/materials.component';
+import { ProfileComponent } from './profile/profile.component';
+import { OrdersComponent } from './orders/orders.component';
 
 
 
 @NgModule({
   declarations: [
-    RouterContainerComponent
+    RouterContainerComponent,
+    HomeComponent,
+    EmployeesComponent,
+    MaterialsComponent,
+    ProfileComponent,
+    OrdersComponent
   ],
   imports: [
     CommonModule
