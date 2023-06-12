8243c00a0ffd85c565a22fdd04ba2293b34a32e2
VedantYadav799 <Vedantyadav799@gmail.com>
2023-06-12 11:30:24 +0530

    new component added

---
+++ frontEnd/src/app/spa/spa.module.ts
@@ -0,0 +1,15 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { RouterContainerComponent } from './router-container/router-container.component';
+
+
+
+@NgModule({
+  declarations: [
+    RouterContainerComponent
+  ],
+  imports: [
+    CommonModule
+  ]
+})
+export class SpaModule { }
