1d2ea5692c8bc5498b440ba0dd3cd5e4b1f27ba1
VedantYadav799 <Vedantyadav799@gmail.com>
2023-07-12 21:28:03 +0530

    new store procedure added

--- Databases/InventoryManagementDB/spdb.sql
+++ Databases/InventoryManagementDB/spdb.sql
@@ -86,3 +86,46 @@ BEGIN
 	DELIMITER ;
     
     
+
+
+
+     DELIMITER $$
+    Create procedure Approved(requestid int,itemid INT,quantity int)
+    begin
+DECLARE status BOOLEAN;
+declare totalcount INT;
+declare shipmentid INT;
+declare val INT;
+DECLARE shipping_cursor CURSOR  FOR SELECT  s.status FROM shippingdetails s WHERE s.shipmentid=(select id from shipments  where shipments.materialrequestid=requestid); 
+set shipmentid=(select id from shipments  where shipments.materialrequestid=requestid);
+
+SELECT COUNT(*) into totalcount FROM shippingdetails s where s.shipmentid=shipmentid;
+update shippingdetails s set s.quantity=quantity, status=1 where s.itemid=itemid;
+set val=0;
+OPEN shipping_cursor ;
+begin
+DECLARE exit_flag INT DEFAULT 0;
+ DECLARE CONTINUE HANDLER FOR SQLSTATE '02000' SET exit_flag = 1;
+shipment:LOOP
+    FETCH shipping_cursor INTO  status;
+    IF exit_flag THEN 
+		LEAVE shipment;
+    END IF;
+        if status=0  then
+		LEAVE shipment;
+	end if;
+        set val=val+1;
+        
+    end loop shipment;
+    end;
+    close shipping_cursor;   
+        if val=totalcount then 
+		update shipments s set s.date=CURRENT_TIMESTAMP where s.materialrequestid=requestid;
+        update materialrequests r set r.status=3 where r.id= requestid;
+        end if;
+    END $$
+DELIMITER ;
+	DELIMITER $$
+    
+  call Approved(4,9,20)
+
