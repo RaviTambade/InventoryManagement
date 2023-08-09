7e9f3cee15c9ef93576df8fd4219150db764a117
VedantYadav799 <Vedantyadav799@gmail.com>
2023-08-10 01:30:38 +0530

    modification in component

--- UI/frontEnd/src/app/request-charts/monthly-report/monthly-report.component.ts
+++ UI/frontEnd/src/app/request-charts/monthly-report/monthly-report.component.ts
@@ -5,7 +5,7 @@ import { RequestService } from 'src/app/spa/request.service';
 import Chart from 'chart.js/auto';
 import { Period } from 'src/app/Period';
 import { DatePipe } from '@angular/common';
-import {  startOfMonth, addDays, format, getISOWeek } from 'date-fns';
+import { startOfMonth, addDays, format, getISOWeek } from 'date-fns';
 
 @Component({
   selector: 'app-monthly-report',
@@ -13,38 +13,34 @@ import {  startOfMonth, addDays, format, getISOWeek } from 'date-fns';
   styleUrls: ['./monthly-report.component.css']
 })
 export class MonthlyReportComponent {
-  selectedWeekValue: string='';
-  public currentDate=new Date();
+  selectedMonthValue: string = '';
   public chart: any;
-  public date1=new Date;
-  public date2=new Date;
-  
-  period:any= {
-    "fromDate":'2023/08/01',
-    "toDate":'2023/08/30'
-  }
-  days: string[] = ["Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday", "Sunday"]
   data: any[] = [];
   report: RequestReport[] = [];
-  month!: string ; // Replace with desired month name
-  year: number = 2023; // Replace with desired year
+  month: number = 0; // Replace with desired month name
+  year: number = 0; // Replace with desired year
   currentMonth: any | undefined; // Use union type with undefined
   currentYear: number | undefined; // Use union type with undefined
-  numWeeks:number | undefined ;
+  numWeeks: number | undefined;
   weekList: string[] = [];
-  constructor(private svc: RequestService, private datePipe:DatePipe) {
+  firstDate = new Date;
+  lastDate = new Date;
+  period: any = {
+    "fromDate": '',
+    "toDate": ''
+  }
+
+  constructor(private svc: RequestService, private datePipe: DatePipe) {
     this.getCurrentMonthAndYear();
     this.calculateWeeks();
-   }
+  }
   ngOnInit(): void {
     this.svc.GetMonthlyReport(12, this.period).subscribe((res) => {
       console.log(res);
       this.report = res;
 
       if (this.report != null) {
-        console.log("in if");
         for (const week of this.weekList) {
-          console.log("for")
           const matchingData = this.report.find((item) => week.includes(item.period));
           if (matchingData) {
             this.data.push(matchingData.requests);
@@ -59,10 +55,6 @@ export class MonthlyReportComponent {
     })
 
   }
-
-
-  
-  
 
   createChart() {
     this.chart = new Chart("MyChart", {
@@ -95,7 +87,7 @@ export class MonthlyReportComponent {
     });
   }
 
-  
+
   getCurrentMonthAndYear() {
     const currentDate = new Date();
     const formattedMonth = this.datePipe.transform(currentDate, 'MMMM');
@@ -103,16 +95,22 @@ export class MonthlyReportComponent {
     this.currentYear = currentDate.getFullYear();
     console.log(this.currentMonth);
     console.log(this.currentYear);
+    const formattedDate = this.datePipe.transform(`${this.currentMonth} 1 2000`, 'M');
+    var month = parseInt(formattedDate || '0');
+    this.selectedMonthValue=`${this.currentYear}-${month}`;
+    console.log(this.selectedMonthValue)
+    this.getFirstAndLastDateOfMonth(this.selectedMonthValue);
+
   }
 
   calculateWeeks() {
-    if(this.currentYear!=undefined){
+    if (this.currentYear != undefined) {
       const lastDayOfMonth = new Date(this.currentYear, this.getMonthIndex(), 0);
       const daysInMonth = lastDayOfMonth.getDate();
       const numWeeks = Math.ceil(daysInMonth / 7);
       this.numWeeks = numWeeks;
       console.log(this.numWeeks);
-      if(this.numWeeks!=undefined){
+      if (this.numWeeks != undefined) {
         this.generateWeekList()
       }
     }
@@ -120,33 +118,19 @@ export class MonthlyReportComponent {
   }
 
   generateWeekList() {
-    if(this.numWeeks!=undefined){
+    if (this.numWeeks != undefined) {
       for (let i = 1; i <= this.numWeeks; i++) {
         this.weekList.push(`week-${i}`);
         console.log(this.weekList)
       }
     }
-   
   }
-  // calculateWeekDates() {
-  //   if(this.currentYear!=undefined){
-  //     const startDate = new Date(this.currentYear, 0, 1); // January 1 of the year
-  //     const daysToAdd = (this.numWeeks - 1) * 7;
-  //     startDate.setDate(startDate.getDate() + daysToAdd);
-  
-  //     this.startDate = startDate;
-  //     this.endDate = new Date(startDate);
-  //     this.endDate.setDate(this.endDate.getDate() + 6); // Adding 6 days for the end of the week
-  //     console.log(this.startDate)
-  //     console.log(this.endDate)
-  //   }
-  // }
 
 
-  
+
   getMonthIndex(): any {
     // Assuming the month names are in English, adjust the index if needed
-    if(this.currentMonth!=undefined){
+    if (this.currentMonth != undefined) {
       const monthNames = [
         'January', 'February', 'March', 'April', 'May', 'June',
         'July', 'August', 'September', 'October', 'November', 'December'
@@ -157,21 +141,20 @@ export class MonthlyReportComponent {
   }
 
 
-  onWeekChange() {
+  onMonthChange() {
     this.chart.destroy();
-    this.report=[];
-    this.data=[];
-    console.log('Selected week:', this.selectedWeekValue);
-    this.getFirstAndLastDateOfWeek(this.selectedWeekValue);
+    this.report = [];
+    this.data = [];
+    console.log('Selected week:', this.selectedMonthValue);
+    this.getFirstAndLastDateOfMonth(this.selectedMonthValue);
 
-    this.svc.GetWeeklyReport(12, this.period).subscribe((res) => {
+    this.svc.GetMonthlyReport(12, this.period).subscribe((res) => {
       console.log(res);
       this.report = res;
+
       if (this.report != null) {
-        console.log("in if");
-        for (const day of this.days) {
-          console.log("for")
-          const matchingData = this.report.find((item) => day.includes(item.period));
+        for (const week of this.weekList) {
+          const matchingData = this.report.find((item) => week.includes(item.period));
           if (matchingData) {
             this.data.push(matchingData.requests);
             console.log(this.data);
@@ -179,30 +162,22 @@ export class MonthlyReportComponent {
             this.data.push(0); // If data not available for the day, use 0
           }
         }
+
         this.createChart();
       }
     })
-    }
+  }
 
 
-   getFirstAndLastDateOfWeek(selectedWeek: string) {
-    const [year, week] = selectedWeek.split('-W').map(Number);
-  
-    // Get the date of the first day of the year
-    const firstDateOfYear = new Date(year, 0, 1);
-  
-    // Find the first day of the selected week (Monday)
-    const firstWeekStart = new Date(firstDateOfYear);
-    const dayOffset = firstWeekStart.getDay() || 7; // Convert Sunday to 7
-    const daysToAdd = (week - 1) * 7 + (dayOffset === 1 ? 0 : 1); // If the first day of the year is Monday, we add 0 days, otherwise, we add 1 day
-    firstWeekStart.setDate(firstWeekStart.getDate() + daysToAdd);
-  
-    // Calculate the last day of the selected week (Sunday)
-    const lastWeekStart = new Date(firstWeekStart);
-    lastWeekStart.setDate(lastWeekStart.getDate() + 6);
-  
-        this.period.fromDate= this.datePipe.transform(firstWeekStart, 'yyyy/MM/dd');
-    this.period.toDate=this.datePipe.transform(lastWeekStart, 'yyyy/MM/dd');
+  getFirstAndLastDateOfMonth(selectedMonth: string) {
+    const [yearStr, monthStr] = selectedMonth.split('-');
+    this.year = parseInt(yearStr);
+    this.month = parseInt(monthStr);
+    this.firstDate = new Date(this.year, this.month - 1, 1);
+    this.lastDate = new Date(this.year, this.month, 0);
+    this.period.fromDate = this.datePipe.transform(this.firstDate, 'yyyy/MM/dd');
+    this.period.toDate = this.datePipe.transform(this.lastDate, 'yyyy/MM/dd');
     console.log(this.period)
   }
+
 }
