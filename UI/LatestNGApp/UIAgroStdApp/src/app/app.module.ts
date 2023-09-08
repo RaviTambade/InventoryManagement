c120ab0ee33462c92a7d5fd948a0f2edfc56260a
VedantYadav799 <Vedantyadav799@gmail.com>
2023-09-08 21:09:39 +0530

    new ng app demo

---
+++ UI/LatestNGApp/UIAgroStdApp/src/app/app.module.ts
@@ -0,0 +1,61 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { HttpClientModule, HTTP_INTERCEPTORS } from '@angular/common/http';
+import { RouterModule } from '@angular/router';
+import { AppComponent } from './app.component';
+import { NavMenuComponent } from './nav-menu/nav-menu.component';
+import { HomeComponent } from './home/home.component';
+import { CounterComponent } from './counter/counter.component';
+import { FetchDataComponent } from './fetch-data/fetch-data.component';
+import { DashboardComponent } from './dashboard/dashboard.component';
+import { LoginComponent } from './authentication/login/login.component';
+import { AuthenticationModule } from './authentication/authentication.module';
+import { JWT_OPTIONS, JwtHelperService } from '@auth0/angular-jwt';
+import { storeInchargeRoutes } from './store-incharge/store-incharge.module';
+import { storeManagerRoutes } from './store-manager/store-manager.module';
+import { storeWorkerRoutes } from './store-worker/store-worker.module';
+import { supervisorRoutes } from './supervisor/supervisor.module';
+import { supervisorInchargeRoutes } from './supervisor-incharge/supervisor-incharge.module';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    NavMenuComponent,
+    HomeComponent,
+    CounterComponent,
+    FetchDataComponent,
+  ],
+  imports: [
+    BrowserModule.withServerTransition({ appId: 'ng-cli-universal' }),
+    HttpClientModule,
+    FormsModule,
+    AuthenticationModule,
+
+    RouterModule.forRoot([
+      { path: '', component: HomeComponent, pathMatch: 'full' },
+      { path: 'counter', component: CounterComponent },
+      { path: 'login', component: LoginComponent },
+      { path: 'dashboard', component: DashboardComponent},
+      {path:'storeincharge',children:storeInchargeRoutes},
+      {path:'storemanager',children:storeManagerRoutes},
+      {path:'storeworker',children:storeWorkerRoutes},
+      {path:'supervisor',children:supervisorRoutes},
+      {path:'supervisorincharge',children:supervisorInchargeRoutes},
+    ])
+  ],
+  providers: [
+    {
+      provide: JWT_OPTIONS,
+      useValue: {
+        tokenGetter: () => {
+          return;
+        },
+        throwNoTokenError: true,
+      },
+    },
+    JwtHelperService,
+  ],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
---
+++ UI/LatestNGApp/UIAgroStdApp/src/app/authentication/login/login.component.ts
@@ -0,0 +1,75 @@
+import { Component } from '@angular/core';
+import { Route, Router } from '@angular/router';
+import { Credential } from 'src/app/Models/credential';
+import { AuthenticationService } from 'src/app/Services/authentication.service';
+import { UserService } from 'src/app/Services/user.service';
+@Component({
+  selector: 'app-login-component',
+  templateUrl: './login.component.html'
+})
+export class LoginComponent {
+  public currentCount = 0;
+  credential: Credential = {
+    contactNumber: '',
+    password: ''
+  }
+  userId: number | undefined;
+  role: any;
+
+  constructor(private router: Router, private authService: AuthenticationService, private userService: UserService) { }
+
+  public onSignIn() {
+    console.log("Validating user");
+    this.authService.validate(this.credential).subscribe((response) => {
+      if (response != null) {
+        localStorage.setItem("JWT", response.token)
+        this.userService.getUserByContact(this.credential.contactNumber).subscribe((response) => {
+          this.userId = response.id;
+          console.log(this.userId);
+          this.userService.getUserRole(this.userId).subscribe((response) => {
+            this.role = response;
+            console.log(this.role);
+             const role=this.role.role;
+            this.navigateByRole(role);
+          })
+        })
+      }
+
+    })
+
+
+  }
+
+  navigateByRole(role: string) {
+    switch (role) {
+      case "Store Incharge":
+        this.router.navigate(["storeincharge/dashboard"])
+        break;
+      case "Store Manager":
+        this.router.navigate(["storemanager/dashboard"])
+        break;
+      case "Supervisor":
+        this.router.navigate(["supervisor/dashboard"])
+        break;
+        case "Supervisor Incharge":
+        this.router.navigate(["supervisorincharge/dashboard"])
+        break;
+      case "Store Worker":
+        this.router.navigate(["storeworker/dashboard"])
+        break;
+    }
+
+
+
+
+
+
+
+
+
+
+
+  }
+
+}
+
