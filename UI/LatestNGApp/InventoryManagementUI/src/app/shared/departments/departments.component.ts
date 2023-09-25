cdd050a21c5d5473a43c6692bb137603b7cfad1b
VedantYadav799 <Vedantyadav799@gmail.com>
2023-09-25 23:54:27 +0530

    unassigned employees name

--- UI/LatestNGApp/InventoryManagementUI/src/app/shared/departments/departments.component.ts
+++ UI/LatestNGApp/InventoryManagementUI/src/app/shared/departments/departments.component.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core';
 import { User } from 'src/app/Models/User';
 import { warehouseStaff } from 'src/app/Models/warehouseStaff';
+import { EmployeeService } from 'src/app/Services/employee.service';
 import { UserService } from 'src/app/Services/user.service';
 import { WarehouseService } from 'src/app/Services/warehouse.service';
 
@@ -13,89 +14,70 @@ export class DepartmentsComponent {
   selectedEmployeeForSwap: any = null; // Store the selected employee
   swap: boolean = false;
   selectSwap: boolean = true;
-  employees:User[]=[]
+  employees:any;
   unassignedEmpids: number[] = [];
   storeManagersids: number[] = [];
   assignedEmpIds:number[]=[];
   warehouses: warehouseStaff[] = []
+
+  role:string="Store Manager"
   data: warehouseStaff[] = []
   users: any[] = [];
   sections:string[]=[];
   selectedSection:string='';
-  constructor(private svc: WarehouseService, private _usrSvc: UserService) {
+  storeManagers:User[]=[];
+  constructor(private svc: WarehouseService, private _usrSvc: UserService,private empSvc:EmployeeService) {
   }
   ngOnInit(): void {
     this.svc.getAllWarehouseStaff().subscribe((res) => {
       console.log(res);
-      this.warehouses = res;
-      this.data=res;
-      console.log(this.warehouses);
-      this.warehouses.forEach(item => {
-          this.sections.push(item.section);
-        this.assignedEmpIds.push(item.employeeId);
-      });
+      this.warehouses=res;
+      this.sections= this.warehouses.map((w) => w.section);
+      this.assignedEmpIds=this.warehouses.map((w)=> w.employeeId)
+      console.log(this.sections)
       console.log(this.assignedEmpIds)
     })
-    this.svc.getAllStoreMangers().subscribe((res) => {
+    this.empSvc.getByRole(this.role).subscribe((res) => {
       this.storeManagersids = res;
       console.log(this.storeManagersids);
       this.unassignedEmpids = this.storeManagersids.filter(id => !this.assignedEmpIds.includes(id));
+      console.log(this.unassignedEmpids)
+ 
       console.log(this.unassignedEmpids);
-        // this.getUnassignedUser(this.unassignedEmpids)
         this.getUser(this.storeManagersids);
 
     })
 
   }
-  onSelectedSection() {
-    console.log(this.selectedSection);
-    const section = this.data.filter(m => m.section === this.selectedSection)
-    console.log(section);
-    this.warehouses = section;
-    console.log(this.warehouses)
-  }
 
-  // getUser(ids: any) {
-  //   this.usrsvc.get(ids).subscribe((res) => {
-  //     console.log(res);
-  //     this.users = res;
-  //     if (this.users.length != 0) {
-  //       for (const user of this.users) {
-  //         const matchingWarehouseStaff = this.warehouses.find(staff => staff.employeeId === user.id);
-  //         if (matchingWarehouseStaff) {
-  //           matchingWarehouseStaff.name = user.name;
-  //         }
-  //       }
-  //       this.getUnassignedEmployees();
-  //     }
-  //   })
 
-  //   console.log(this.warehouses)
-  // }
   getUser(ids:any) {
-    const userIds = this.data.map(item => item.employeeId).filter((value, index, self) => self.indexOf(value) === index); // Filter duplicates
     
     for (const userId of ids) {
       this._usrSvc.getUser(userId).subscribe(data => {
+        this.storeManagers.push(data);
         for (const responseItem of data) {
-          const users = this.data.filter(u => u.employeeId === responseItem.id);
+          const users = this.warehouses.filter(u => u.employeeId === responseItem.id);
           for (const user of users) {
             user.name = responseItem.name;
           }
         }
+        this.getUnassignedEmployees();
       });
     }
-  }
-  getUnassignedUser(ids:any) {
-    
-    for (const userId of ids) {
-      this._usrSvc.getUser(userId).subscribe(data => {
-       this.employees.push(data);
-      });
-    }
-    console.log(this.employees)
+
+    console.log(this.storeManagers);
+    console.log(this.data)
   }
 
+  getUnassignedEmployees(){
+   this.unassignedEmpids.forEach(element => {
+    const employee = this.storeManagers.find(manager => manager.id === element);
+    
+    console.log(employee)
+     });
+
+  }
 
 
   selectEmployeeForSwap(employee: any) {
@@ -149,6 +131,13 @@ export class DepartmentsComponent {
     this.svc.updateWarehouseStaff(modifiedData).subscribe((res)=>{
       console.log(res);
     })
+  }
+
+  onSelectedSection() {
+    console.log(this.selectedSection);
+    const section = this.data.filter(m => m.section === this.selectedSection)
+    console.log(section);
+    this.warehouses = section;
   }
 }
 
