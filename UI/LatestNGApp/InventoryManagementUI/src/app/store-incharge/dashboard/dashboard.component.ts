ba5f9d02a38056bf5529355a216d581dc87d4b5a
VedantYadav799 <Vedantyadav799@gmail.com>
2023-09-26 23:58:13 +0530

    modification in dashboard

--- UI/LatestNGApp/InventoryManagementUI/src/app/store-incharge/dashboard/dashboard.component.ts
+++ UI/LatestNGApp/InventoryManagementUI/src/app/store-incharge/dashboard/dashboard.component.ts
@@ -7,4 +7,9 @@ import { Component } from '@angular/core';
 })
 export class DashboardComponent {
   showAdditionalData = false;
+  showEmployeesData = false;
+  showInventoryData = false;
+
+  
+
 }
