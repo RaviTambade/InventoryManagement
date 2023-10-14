235e185e97408d96996c442aa69154c8b302fbc7
PragatiBangar <bangarpragati11@gmail.com>
2023-10-14 16:33:34 +0530

    changes in store incharge Module

---
+++ UI/LatestNGApp/InventoryManagementUI/src/app/Services/dashboard.service.ts
@@ -0,0 +1,46 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class DashboardService {
+
+  constructor(private http:HttpClient) { }
+
+  getRequestCount(date:number,supervisorId:number):Observable<any>{
+    let url ="http://localhost:5161/api/bi/requestcount/" +date+ "/"+supervisorId ;
+    return this.http.get<any>(url);
+  }
+
+  getOrderCount(date:number,storeManagerId:number):Observable<any>{
+    let url ="http://localhost:5161/api/bi/ordercount/" +date+ "/"+storeManagerId ;
+    return this.http.get<any>(url);
+  }
+
+  getRequestByStatus():Observable<any>{
+    let url ="http://localhost:5161/api/bi/requests"  ;
+    return this.http.get<any>(url);
+  }
+  
+  getSupervisors():Observable<any>{
+    let url ="http://localhost:5161/api/bi/supervisors" ;
+    return this.http.get<any>(url);
+  }
+
+  getTotalAndCounts():Observable<any>{
+    let url ="http://localhost:5161/api/bi/materials" ;
+    return this.http.get<any>(url);
+  }
+
+  getMaterialRequestBySupervisor(supervisorId:number):Observable<any>{
+    let url ="http://localhost:5161/api/bi/supervisorrequest/" +supervisorId ;
+    return this.http.get<any>(url);
+  }
+
+  getAllOrdersCount():Observable<any>{
+    let url ="http://localhost:5161/api/bi/allorderstatus" ;
+    return this.http.get<any>(url);
+  }
+}
--- UI/LatestNGApp/InventoryManagementUI/src/app/store-incharge/dashboard/dashboard.component.ts
+++ UI/LatestNGApp/InventoryManagementUI/src/app/store-incharge/dashboard/dashboard.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { DashboardService } from 'src/app/Services/dashboard.service';
 import { EmployeeService } from 'src/app/Services/employee.service';
 
 @Component({
@@ -12,13 +13,32 @@ export class DashboardComponent {
   showInventoryData = false;
   showTodaysOrders = false;
   showAllOrders = false;
+  order:any;
+  count:any;
 
-  constructor(private _empSvc:EmployeeService){
+  constructor(private _empSvc:EmployeeService,private svc:DashboardService){
 
   }
   ngOnInit():void{
     this._empSvc.getEmployees().subscribe((res)=>{
       console.log(res)
+    })
+    this.getAllOrders();
+    this.getTotalAndCounts();
+
+  }
+
+  getAllOrders(){
+    this.svc.getAllOrdersCount().subscribe((res)=>{
+     this.order=res;
+     console.log(res);
+    })
+  }
+
+  getTotalAndCounts(){
+    this.svc.getTotalAndCounts().subscribe((res)=>{
+     this.count=res;
+     console.log(res);
     })
   }
 
